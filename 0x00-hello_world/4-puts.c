f0bd54b2cd0397ea27f574fa78204a45e7ee63d3
DavidOngora <davidongora@gmail.com>
2023-09-07 13:26:44 -0700

    solutions for the taks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main  -entry point
+ * 
+ * description -uses put method to print output
+ *
+ * return: int
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
