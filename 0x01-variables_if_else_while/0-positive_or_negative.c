d5df0ff808f45321524feb6af4e2a7803547b700
DavidOngora <davidongora@gmail.com>
2023-09-08 12:56:39 -0700

    removed the bug extra bracket

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,20 +13,20 @@
 
 int main(void)
 {
-	int n, digit;
+	int n, lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/*my code*/
-	digit = n % 10; /*gets last digit*/
+	lastDigit = n % 10; /*gets last digit*/
 
-	if (digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	if (lastDigit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, lastDigit);
+	else if (lastDigit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, lastDigit);
+	else if (lastDigit < 6 && lastDigit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastDigit);
 
 	return (0);
 }
