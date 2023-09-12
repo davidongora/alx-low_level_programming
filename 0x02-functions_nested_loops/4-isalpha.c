d31ab837bb82b9ad6588f37fa5e11bdfa5cb10c4
DavidOngora <davidongora@gmail.com>
2023-09-12 12:48:38 -0700

    betty formated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,15 +3,14 @@
 /**
  * _isalpha - checks if character is a letter
  *            both lowercase or uppercase
- *
  * @c: takes input from other functions.
  *
  * Return: 1 is c if true else 0
-*/
+ */
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c<= 'z') || ( c >= 'A' &&
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' &&
 	    c <= 'Z'))
 		return (1);
 	return (0);
