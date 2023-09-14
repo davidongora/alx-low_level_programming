01e432de7b35f564c8a6535ce41f82b4432e9d63
DavidOngora <davidongora@gmail.com>
2023-09-14 14:23:02 -0700

    solved

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,17 +12,13 @@ void print_diagonal(int n)
 	int postn, space;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
-	
+	else
+	{
 		for (postn = 1; postn <= n; ++postn)
 		{
-			for (space = 0; space <= postn; ++space)
-			{
+			for (space = 1; space <= postn; ++space)
 				_putchar(' ');
-			}
 			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
 		}
