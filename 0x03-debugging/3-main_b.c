e9653f343bac85c8fb5309ee034b275000449b98
DavidOngora <davidongora@gmail.com>
2023-09-14 12:22:49 -0700

    changes

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,4 +1,4 @@
-#include "../main.h"
+#include "main.h"
 
 /**
 * main - takes a date and prints how many days are left in the year, taking
@@ -12,9 +12,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 2;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
