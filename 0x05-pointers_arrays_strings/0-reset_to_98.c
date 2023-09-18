12f013ce62d27b181a89dd024281c01bb27012f1
DavidOngora <davidongora@gmail.com>
2023-09-18 13:18:50 -0700

    created pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset to 98 : as an int parameter as a pointer and updates its value to 98
+ * main: first execution
+ * return: 0 always a success output
+ */
+
+ void reset_to_98(int *n)
+{
+	*n =98;
+}
