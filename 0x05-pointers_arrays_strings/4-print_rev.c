784c5bca8e0bbaa9811bae689f8e14a11aec142d
DavidOngora <davidongora@gmail.com>
2023-09-18 14:21:00 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,9 @@ void print_rev(char *s)
     int length = 0;
     int i;
 
-    // Calculate the length of the string
     while (s[length] != '\0')
         length++;
 
-    // Print the string in reverse
     for (i = length - 1; i >= 0; i--)
         _putchar(s[i]);
 
