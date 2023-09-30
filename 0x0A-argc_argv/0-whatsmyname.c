7ac1a8374639c207ba542146693fad826a174c2c
DavidOngora <davidongora@gmail.com>
2023-09-29 19:37:14 -0700

    added corections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
  * @argv: program array commands
  * Return: o if successful
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	(void) argc;
-	printf("%s\n", argv[0]);
+	int i =0;
+
+	printf("%s\n", argv[i]);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,16 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int m;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int product = num1 * num2;
-
-	printf("%d\n", product);
+	else
+	{
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
+	}
 	return (0);
 }
