e3073e9be35f3f8c28e8358695da2c82f586ad67
DavidOngora <davidongora@gmail.com>
2023-09-29 19:24:44 -0700

    solutions for the argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints out its program
+ * @argc: program commands
+ * @argv: program array commands
+ * Return: o if successful
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
