758e9ee841e295ef50fe7f9a8efe46bbd03ba73c
DavidOngora <davidongora@gmail.com>
2023-10-03 00:51:55 -0700

    removed the extra lines

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,6 @@
 #include <stdlib.h>
 
 /**
- * free_grid -  free up a 2d array grid
- *
- * @grid: double pointer 2d grid
- * @height: height of grid
- *
- * Return: nothing
-*/
-
-void free_grid(int **grid, int height)
-{
-	#include <stdlib.h>
-
-/**
  * free_grid - frees the memory allocate for the grid
  * created by alloc_grid()
  * @grid: grid to free
