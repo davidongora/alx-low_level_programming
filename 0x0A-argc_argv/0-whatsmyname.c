571558158eb29d6a97aa587052fea0aaac26aa88
DavidOngora <davidongora@gmail.com>
2023-10-03 01:02:05 -0700

    removed trailing wait spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,13 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
  * main- a program that prints its name
- *
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
- *
  * Return: Always 0 (Success)
 */
-
 int main(int argc __attribute__((unused)), char *argv[])
 {
-        printf("%s\n", *argv);
-
-        return (0);
+	printf("%s\n", *argv);
+	return (0);
 }
