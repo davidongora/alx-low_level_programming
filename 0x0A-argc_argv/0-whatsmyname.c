8996671936c9be3f97f942a2503fbe043bd732d0
DavidOngora <davidongora@gmail.com>
2023-10-03 01:10:48 -0700

    modifications

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,9 @@
  * @argv: array pointer that holds the arguments passed
  * Return: Always 0 (Success)
 */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i = 0;
-
-	printf("%s\n", argv[i]);
+	(void) argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
