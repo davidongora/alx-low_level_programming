536fb16b44c54f764e587352a956ff9e3afc231c
DavidOngora <davidongora@gmail.com>
2023-10-23 23:28:57 -0700

    sucess

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,31 +9,32 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  listint_t *current_node, *previous_node;
+	listint_t *temp = *head;
+	listint_t *current = NULL;
+	unsigned int i = 0;
 
-  if (*head == NULL || index == 0) {
-    // Delete the head node.
-    current_node = *head;
-    *head = (*head)->next;
-    free(current_node);
-    return 1;
-  }
+	if (*head == NULL)
+		return (-1);
 
-  // Find the previous node to the node to be deleted.
-  previous_node = *head;
-  current_node = (*head)->next;
-  for (unsigned int i = 1; i < index; i++) {
-    if (current_node == NULL) {
-      // The index is out of bounds.
-      return -1;
-    }
-    previous_node = current_node;
-    current_node = current_node->next;
-  }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
 
-  // Delete the node.
-  previous_node->next = current_node->next;
-  free(current_node);
+	while (i < index - 1)
+	{
+		if (!temp || !(temp->next))
+			return (-1);
+		temp = temp->next;
+		i++;
+	}
 
-  return 1;
+
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
+
+	return (1);
 }
